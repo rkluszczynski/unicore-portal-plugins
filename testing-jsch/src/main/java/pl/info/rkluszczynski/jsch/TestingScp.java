c3f065c303d965741856364339e77b6e24540fe0
Rafal Kluszczynski <rafal.kluszczynski@allegro.pl>
2014-07-08 02:30:11 +0200

    adding jsch testing submodule

---
+++ testing-jsch/src/main/java/pl/info/rkluszczynski/jsch/TestingScp.java
@@ -0,0 +1,164 @@
+package pl.info.rkluszczynski.jsch;
+
+/* -*-mode:java; c-basic-offset:2; indent-tabs-mode:nil -*- */
+/**
+ * This program will demonstrate the file transfer from local to remote.
+ *   $ CLASSPATH=.:../build javac ScpTo.java
+ *   $ CLASSPATH=.:../build java ScpTo file1 user@remotehost:file2
+ * You will be asked passwd.
+ * If everything works fine, a local file 'file1' will copied to
+ * 'file2' on 'remotehost'.
+ *
+ */
+
+import com.jcraft.jsch.*;
+
+import java.io.*;
+
+public class TestingScp {
+    public static void main(String[] arg) throws JSchException, IOException {
+        System.out.println(System.getProperty("user.dir"));
+
+        String localFile = "testing-jsch/src/main/resources/test.txt";
+        String user = "klusi";
+        String host = "alfred.mat.umk.pl";
+        String remoteFilename = "jschScpTest.txt";
+
+        final String passwd = "";
+
+        JSch jsch = new JSch();
+        Session session = jsch.getSession(user, host, 22);
+//        session.setPassword(passwd);
+        session.setUserInfo(new UserInfo() {
+            @Override
+            public String getPassphrase() {
+                return null;
+            }
+
+            @Override
+            public String getPassword() {
+                return passwd;
+            }
+
+            @Override
+            public boolean promptPassword(String message) {
+                return true;
+            }
+
+            @Override
+            public boolean promptPassphrase(String message) {
+                return false;
+            }
+
+            @Override
+            public boolean promptYesNo(String message) {
+                return true;
+            }
+
+            @Override
+            public void showMessage(String message) {
+
+            }
+        });
+        session.connect();
+
+        boolean ptimestamp = true;
+
+        // exec 'scp -t rfile' remotely
+        String command = "scp " + (ptimestamp ? "-p" : "") + " -t " + remoteFilename;
+        Channel channel = session.openChannel("exec");
+        ((ChannelExec) channel).setCommand(command);
+
+        // get I/O streams for remote scp
+        OutputStream out = channel.getOutputStream();
+        InputStream in = channel.getInputStream();
+
+        channel.connect();
+
+
+        if (checkAck(in) != 0) {
+            System.exit(0);
+        }
+
+        File _lfile = new File(localFile);
+
+        if (ptimestamp) {
+            command = "T " + (_lfile.lastModified() / 1000) + " 0";
+            // The access time should be sent here,
+            // but it is not accessible with JavaAPI ;-<
+            command += (" " + (_lfile.lastModified() / 1000) + " 0\n");
+            out.write(command.getBytes());
+            out.flush();
+            if (checkAck(in) != 0) {
+                System.exit(0);
+            }
+        }
+
+        // send "C0644 filesize filename", where filename should not include '/'
+        long filesize = _lfile.length();
+        command = "C0644 " + filesize + " ";
+        if (localFile.lastIndexOf('/') > 0) {
+            command += localFile.substring(localFile.lastIndexOf('/') + 1);
+        } else {
+            command += localFile;
+        }
+        command += "\n";
+        out.write(command.getBytes());
+        out.flush();
+        if (checkAck(in) != 0) {
+            System.exit(0);
+        }
+
+        // send a content of lfile
+        FileInputStream fis = new FileInputStream(localFile);
+        byte[] buf = new byte[1024];
+        while (true) {
+            int len = fis.read(buf, 0, buf.length);
+            if (len <= 0) break;
+            out.write(buf, 0, len); //out.flush();
+        }
+        fis.close();
+        fis = null;
+        // send '\0'
+        buf[0] = 0;
+        out.write(buf, 0, 1);
+        out.flush();
+        if (checkAck(in) != 0) {
+            System.exit(0);
+        }
+        out.close();
+
+        channel.disconnect();
+        session.disconnect();
+
+        System.exit(0);
+    }
+
+    static int checkAck(InputStream in) throws IOException {
+        int b = in.read();
+        // b may be 0 for success,
+        //          1 for error,
+        //          2 for fatal error,
+        //          -1
+        if (b == 0) return b;
+        if (b == -1) return b;
+
+        if (b == 1 || b == 2) {
+            StringBuffer sb = new StringBuffer();
+            int c;
+            do {
+                c = in.read();
+                sb.append((char) c);
+            }
+            while (c != '\n');
+            if (b == 1) { // error
+                System.out.print(sb.toString());
+            }
+            if (b == 2) { // fatal error
+                System.out.print(sb.toString());
+            }
+        }
+        return b;
+    }
+
+}
