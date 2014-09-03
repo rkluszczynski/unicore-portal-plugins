95ac489a7cf2f25eca8ce71494b3a30b385955b7
Rafal Kluszczynski <klusi@icm.edu.pl>
2014-09-03 12:51:16 +0200

    update implementation to portal in version 1.1.0

--- pl.plgrid.unicore.common/src/main/java/pl/plgrid/unicore/common/ui/files/InputFilePanelCallback.java
+++ pl.plgrid.unicore.common/src/main/java/pl/plgrid/unicore/common/ui/files/InputFilePanelCallback.java
@@ -2,14 +2,14 @@ package pl.plgrid.unicore.common.ui.files;
 
 import com.vaadin.ui.TextField;
 import eu.unicore.portal.grid.core.nodes.Node;
-import eu.unicore.portal.grid.ui.browser.StorageChooser;
+import eu.unicore.portal.grid.ui.browser.GridStorageChooser;
 import org.apache.log4j.Logger;
 
 /**
  * @author rkluszczynski
  */
 public class InputFilePanelCallback implements
-        StorageChooser.StorageChooserCallback {
+        GridStorageChooser.StorageChooserCallback {
 
     private static final Logger logger = Logger
             .getLogger(InputFilePanelCallback.class);
--- pl.plgrid.unicore.common/src/main/java/pl/plgrid/unicore/common/ui/nodes/FileInStorageChooser.java
+++ pl.plgrid.unicore.common/src/main/java/pl/plgrid/unicore/common/ui/nodes/FileInStorageChooser.java
@@ -1,13 +1,13 @@
 package pl.plgrid.unicore.common.ui.nodes;
 
 import eu.unicore.portal.grid.core.nodes.u6.FileNode;
-import eu.unicore.portal.grid.ui.browser.StorageChooser;
+import eu.unicore.portal.grid.ui.browser.GridStorageChooser;
 
 /**
  * @author rkluszczynski
  */
 @SuppressWarnings("serial")
-public class FileInStorageChooser extends StorageChooser {
+public class FileInStorageChooser extends GridStorageChooser {
 
     public FileInStorageChooser(String caption, StorageChooserCallback callback) {
         super(caption, callback);
